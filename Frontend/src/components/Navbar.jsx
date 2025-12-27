e52ec00467c053ad31490d05b9bcddbcd9ad9c16
Aditya-Jaiswar-8796 <adityajaiswar8796@gmail.com>
2025-12-27 15:56:22 +0530

    responsive

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = (props) => {
     <div className={`${props.light ? "text-blue-950 bg-[#6db5fd]" : "text-white bg-[#080a2e]"} shadow-lg shadow-blue-800 absolute w-full h-16 flex items-center px-8 justify-between`}>
       <div className="font-lobo text-center flex gap-2">{props.light ? <img width={44} height={44} src="/file2.gif" alt="logo" /> : <img width={44} height={44} src="/file.gif" alt="logo" />} <span className='font-bold text-xl mt-2
       '>QShareX</span></div>
-      <div className="icons flex">
+      <div className="icons flex max-[406px]:flex-col max-[406px]:pt-10  max-[406px]:gap-1  max-[406px]:items-end">
         <button onClick={upload} type="button" className={props.light ? `hover:scale-105 transition-all duration-150 text-white font-semibold bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80  rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 ` : "hover:scale-105 transition-all duration-150 text-blue-900 font-semibold  bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80  rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"}>
           Start Sharing
         </button>
